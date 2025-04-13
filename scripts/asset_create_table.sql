c16ee7eed103ebe1edcc9b47796aab3ca93f892e
Kartik Kulkarni <kartik.kulkarni@outlook.com>
2025-04-14 00:03:58 +0530

    Removed Unnecessary Complexity

--- scripts/asset_create_table.sql
+++ scripts/asset_create_table.sql
@@ -1,4 +1,3 @@
-USE AssetManagement;
 /*  For an Activity funds will be allocated accordingly. 
     The available lab funds will be updated and data will be logged.
     To carry out activity using some items, Request will be created.
@@ -27,22 +26,19 @@ USE AssetManagement;
     We will not maintain supplier information in the database for purchases. Purchase order has been distilled as per requirement.
 */
 
-/*
-DROP TABLE IF EXISTS ItemSupplier;
-DROP TABLE IF EXISTS Supplier;
-*/
+USE AssetManagement;
+
+DROP TABLE IF EXISTS ActivityItemTransaction;
 DROP TABLE IF EXISTS AssetTransactionLog;
 DROP TABLE IF EXISTS Asset;
 DROP TABLE IF EXISTS POLog;
 DROP TABLE IF EXISTS POItem;
 DROP TABLE IF EXISTS PurchaseOrder;
-DROP TABLE IF EXISTS ActivityItemTransaction;
-DROP TABLE IF EXISTS LabActivityLog;
 DROP TABLE IF EXISTS RequestLog;
 DROP TABLE IF EXISTS RequestItem;
 DROP TABLE IF EXISTS Request;
+DROP TABLE IF EXISTS LabActivityLog;
 DROP TABLE IF EXISTS LabActivity;
-DROP TABLE IF EXISTS LabItemStatus;
 DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS LabLog;
 DROP TABLE IF EXISTS UserRole;
@@ -53,7 +49,7 @@ DROP TABLE IF EXISTS Roles;
 
 CREATE TABLE Roles (
     RoleId INT NOT NULL AUTO_INCREMENT,
-    RoleName VARCHAR(50), /* HOD, Lab incharge, Lab assistant, Student */ 
+    RoleName VARCHAR(50), /* Admin, HOD, Lab incharge, Lab assistant, Student */ 
     RoleDesc VARCHAR(100),
 	DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     IsActive BOOLEAN,
@@ -73,8 +69,8 @@ CREATE TABLE Users (
 	Email VARCHAR(50),
     Contact VARCHAR(50),
     UserAddress VARCHAR(100),
-	DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     IsAppUser BOOLEAN, /* for Student it is false. Student can not log in in the application*/
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     IsActive BOOLEAN,
     CONSTRAINT pk_u PRIMARY KEY (UserId)
 );
@@ -83,6 +79,8 @@ CREATE TABLE Department (
     DeptId INT NOT NULL AUTO_INCREMENT,
     DeptName VARCHAR(50),
     DeptDesc VARCHAR(100),
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    IsActive BOOLEAN,
     CONSTRAINT pk_d PRIMARY KEY (DeptId)
 );
 
@@ -96,6 +94,7 @@ CREATE TABLE Lab (
     LabName VARCHAR(50),
     FundsAvailable FLOAT, 
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
     IsActive BOOLEAN,
     CONSTRAINT pk_l PRIMARY KEY (LabId),
     CONSTRAINT fk_l_DeptId FOREIGN KEY (DeptId) REFERENCES  Department (DeptId)
@@ -108,7 +107,8 @@ CREATE TABLE UserRole (
     RoleId INT,
     DateJoined DATETIME,
     DateLeft DATETIME,
-    IsActive BOOLEAN,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    IsActive BOOLEAN, /*Required or not?*/
     CONSTRAINT pk_ur PRIMARY KEY (UserRoleId),
     CONSTRAINT fk_ur_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId),
     CONSTRAINT fk_ur_LabId FOREIGN KEY (LabId) REFERENCES  Lab (LabId), 
@@ -116,48 +116,32 @@ CREATE TABLE UserRole (
 );
 
 CREATE TABLE LabLog (
-    /* There will only be inserts in this table, no updation or deletion against fund transaction done in lab table.*/
+     /* There will only be inserts in this table, no updation or deletion against fund transaction done in lab table.*/
     LabLogId INT NOT NULL AUTO_INCREMENT,
     LabId INT,
-    UserId INT, /* UserId who modified lab record for funds */
-    ActionTaken VARCHAR(20), /* Added, Spent */
+    ActionTakenBy INT, /* UserId who modified lab record for funds (e.g. 'HOD','Lab Incharge') */
+    ActionTaken VARCHAR(20), /* AAdded, Spent, Refund/ */
     ActionDescription VARCHAR(100),	
     Amount FLOAT, /*Funds*/
     DateCreated DATETIME ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_ll PRIMARY KEY (LabLogId),
     CONSTRAINT fk_ll_LabId FOREIGN KEY (LabId) REFERENCES  Lab (LabId),
-    CONSTRAINT fk_ll_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId)
+    CONSTRAINT fk_ll_UserId FOREIGN KEY (ActionTakenBy) REFERENCES  Users (UserId)
 );
 
 CREATE TABLE Item (
-    /* Stores only unique items. And you should check to not allow for duplicate entries. TODO: Add unique constraints. Assume cost of the item is fixed for all time. If the cost changes, then we will need to add log to maintain history of prices for that item, so that correspondingly every project will have the correct price by correct linking with the purchase order. */
+     /* Stores only unique items. And you should check to not allow for duplicate entries. TODO: Add unique constraints. Assume cost of the item is fixed for all time. If the cost changes, then we will need to add log to maintain history of prices for that item, so that correspondingly every project will have the correct price by correct linking with the purchase order. */
     ItemId INT NOT NULL AUTO_INCREMENT,
-    SerialNo VARCHAR(20),
     Category VARCHAR(20), /* Monitor, Keyboard, CPU, Cards, Camera, Motors, etc*/
 	Make VARCHAR(100),
     Model VARCHAR(100),
-    PurchaseDate DATETIME,
-	WarrantyEndDate DATETIME,
-    Cost FLOAT,
+    WarrantyPeriodMonths INT,
     ItemDescription VARCHAR(100), /* Useful for items for which a box is purchased */
-	CreatedBy INT, /* UserId*/
+	CreatedBy INT, /* UserId who created this record*/
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     IsActive BOOLEAN,
-    CONSTRAINT pk_i PRIMARY KEY (ItemId)
-);
-
-/* Separated for efficiency for updation and */
-CREATE TABLE LabItemStatus (
-    LabItemId INT NOT NULL AUTO_INCREMENT,
-    LabId INT,
-    ItemId INT,
-    ItemCondition VARCHAR(20), /* Good, Under Repair, scraped, lost*/ 
-    StatusDescription VARCHAR(100), 
-    CreatedBy INT, /* UserId*/
-    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_lis PRIMARY KEY (LabItemId),
-    CONSTRAINT fk_lis_ItemId FOREIGN KEY (ItemId) REFERENCES  Item (ItemId),
-    CONSTRAINT fk_lis_LabId FOREIGN KEY (LabId) REFERENCES  Lab (LabId)
+    CONSTRAINT pk_i PRIMARY KEY (ItemId),
+    CONSTRAINT fk_i_UserId FOREIGN KEY (CreatedBy) REFERENCES  Users (UserId)
 );
 
 CREATE TABLE LabActivity (
@@ -166,103 +150,86 @@ CREATE TABLE LabActivity (
     If the funds are not sufficient, then Request/Purchase order must get rejected.
     If the activity is closed, the funds available should get added to lab funds.
     Once more funds are allocated, the funds available and allocated should be updated equally.
-    Note that only the spent amount from the activity should be deducted from the lab funds.
-    FundsAllocated is only used for planning and can be changed as required.  
+    Lab funds when allocated to Activity then it is subtracted from Lab funds,  
+    and when purchase is made against the activity, 
+    then it is subtracted from activity funds and available funds will be updated.
     */  
     ActivityId INT NOT NULL AUTO_INCREMENT,
     LabId INT,
-    UserId INT, /*who created */
+    InitiatorId INT, /*UserId who is responsible for the activity, e.g.'Lab Incharge'*/
     ActivityType VARCHAR(20), /* Research, Project, Lab, other, etc */ 
     ActivityDescription VARCHAR(100), /* project code, name, also for other status*/
     FundsAllocated  FLOAT, /* Total Purchase order amount for this activity should not exceed this amount */
     FundsAvailable FLOAT, 
     StartDate DATETIME,
     EndDate DATETIME,
-    IsActive BOOLEAN,
+    IsClosed BOOLEAN, /* If activity closed then value should be 1 else 0 */
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
 	DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_la PRIMARY KEY (ActivityId),
     CONSTRAINT fk_la_LabId FOREIGN KEY (LabId) REFERENCES  Lab (LabId),
-    CONSTRAINT fk_la_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId)
+    CONSTRAINT fk_la_UserId FOREIGN KEY (InitiatorId) REFERENCES  Users (UserId)
+);
+
+CREATE TABLE LabActivityLog (
+     /* Once the item is issued or returned, it must be reflected in Asset table for total quantity available for that item */
+    ActivityLogId INT NOT NULL AUTO_INCREMENT,
+    ActivityId INT,
+    ActionTakenBy INT, /* (UserId) Processed by  e.g.'Lab Incharge'*/
+    ActionTaken VARCHAR(20), /* Added, Spent, Refund */
+    ActionDescription VARCHAR(100),	
+    Funds FLOAT,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT pk_lal PRIMARY KEY (ActivityLogId),
+    CONSTRAINT fk_lal_ActivityId FOREIGN KEY (ActivityId) REFERENCES  LabActivity (ActivityId),
+    CONSTRAINT fk_lal_UserId FOREIGN KEY (ActionTakenBy) REFERENCES  Users (UserId)
 );
 
 CREATE TABLE Request (
     RequestId INT NOT NULL AUTO_INCREMENT,
     ActivityID Int,
     RequestDate DATETIME,   
-    UserId INT,  /*User id of the person (student, professor) who created request for items for the activity */
+    Requestor INT,  /*User id of the person (student, professor) who created request for items for the activity */
     RequestStatus VARCHAR(50), /* Generated, Approved, Rejected, Complete, other */
     IsActive BOOLEAN,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_rq PRIMARY KEY (RequestId),
     CONSTRAINT fk_rq_ActivityId FOREIGN KEY (ActivityID) REFERENCES  LabActivity (ActivityID),
-    CONSTRAINT fk_rq_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId)
+    CONSTRAINT fk_rq_UserId FOREIGN KEY (Requestor) REFERENCES  Users (UserId)
 );
 
 CREATE TABLE RequestItem (
+    /* if item is not listed in item list,
+     then create the item in item table and then allocate in requestItem table*/
     RItemID INT NOT NULL AUTO_INCREMENT,
     RequestId INT,
-    Category VARCHAR(20), /*Monitor, Keyboard, CPU, Camera etc*/
-	Make VARCHAR(100),
-    Model VARCHAR(100),
+    ItemId INT,
+    IsUnknown BOOLEAN, /* If the item is not listed in item list, then it will be unknown*/
     QuantityRequested INT,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_ri PRIMARY KEY (RItemID),
+    CONSTRAINT fk_ri_ItemId FOREIGN KEY (ItemId) REFERENCES  Item (ItemId),
     CONSTRAINT fk_ri_RequestId FOREIGN KEY (RequestId) REFERENCES  Request (RequestId)
 );
   
 CREATE TABLE RequestLog (
     RequestLogId INT NOT NULL AUTO_INCREMENT,    
     RequestId INT,
-    UserId INT,  /* Processed by */
+    ActionTakenBy INT,  /* UserId Processed by e.g. 'Lab Assistance, 'Lab Incharge''*/
     RequestStatus VARCHAR(50), /* Generated, Approved, Rejected, Complete, other */
     RequestDescription VARCHAR(100), 
-    StatusUpdateDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_ril PRIMARY KEY (RequestLogId),
     CONSTRAINT fk_ril_RequestId FOREIGN KEY (RequestId) REFERENCES  Request (RequestId),
-    CONSTRAINT fk_ril_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId)
-);
-
-CREATE TABLE LabActivityLog (
-    /* Once the item is issued or returned, it must be reflected in Asset table for total quantity available for that item */
-    ActivityLogId INT NOT NULL AUTO_INCREMENT,
-    ActivityId INT,
-    UserId INT, /* Processed by */
-    ActionTaken VARCHAR(20), /* Added, Spent */
-    ActionDescription VARCHAR(100),	
-    Funds FLOAT,
-    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT pk_lal PRIMARY KEY (ActivityLogId),
-    CONSTRAINT fk_lal_ActivityId FOREIGN KEY (ActivityId) REFERENCES  LabActivity (ActivityId),
-    CONSTRAINT fk_lal_UserId FOREIGN KEY (IssuedUserId) REFERENCES  Users (UserId)
-);
-
-CREATE TABLE ActivityItemTransaction (
-     /* Once the item is issued or returned, it must be reflected in Asset table for total quantity available for that item */
-    ActivityTrasId INT NOT NULL AUTO_INCREMENT,
-    ActivityId INT,
-    RItemID INT,
-    ItemId INT,
-    IssuedUserId INT, /* Project Leader/Individual*/ 
-    IssuedDate DATETIME,
-    IssuedQuantity INT,  
-    ReceiveDate DATETIME,
-    ReceivedQuantity INT,
- /*   QuantityDamaged INT,
-    QuantityLost INT,*/
-    ShortDescription VARCHAR(50),	
-    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
-	DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT pk_ait PRIMARY KEY (ActivityTrasId),
-    CONSTRAINT fk_ait_ActivityId FOREIGN KEY (ActivityId) REFERENCES  LabActivity (ActivityId),
-    CONSTRAINT fk_ait_RItemId FOREIGN KEY (RItemID) REFERENCES  RequestItem (RItemID),
-    CONSTRAINT fk_ait_ItemId FOREIGN KEY (ItemId) REFERENCES  Item (ItemId),
-    CONSTRAINT fk_ait_UserId FOREIGN KEY (IssuedUserId) REFERENCES  Users (UserId)
+    CONSTRAINT fk_ril_UserId FOREIGN KEY (ActionTakenBy) REFERENCES  Users (UserId)
 );
 
 CREATE TABLE PurchaseOrder (
     POId INT NOT NULL AUTO_INCREMENT,
     ActivityId  INT,
     OrderDate DATETIME,
-	Amount FLOAT,
+	Amount FLOAT, /* total cost of all items (cost of one item= quantity*costperunit)*/
     POStatus VARCHAR(50),
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
@@ -274,25 +241,25 @@ CREATE TABLE PurchaseOrder (
 CREATE TABLE POItem (
     POItemId INT NOT NULL AUTO_INCREMENT,
     POId INT,
-    Category VARCHAR(20), /*Monitor, Keyboard, CPU, Resistors etc*/
-	Make VARCHAR(100),
-    Model VARCHAR(100),
+    ItemId INT,
     QuantityOrdered INT,
 	CostPerUnit FLOAT,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_poi PRIMARY KEY (POItemId),
+    CONSTRAINT fk_poi_ItemId FOREIGN KEY (ItemId) REFERENCES  Item (ItemId),
     CONSTRAINT fk_poi_POId FOREIGN KEY (POId) REFERENCES  PurchaseOrder (POId)
 );
 
 CREATE TABLE POLog (
     POLogId INT NOT NULL AUTO_INCREMENT,
     POId INT,
-    UserId INT, /*User who updated status */ 
+    POCreatedBy INT, /*UserId who created/updated PO Lab Assistance/incharge  */ 
     POStatus VARCHAR(50), /* Generated, Approved, Rejected, Received/Complete, Other */
     POStatusDescription VARCHAR(100), /* Fill when status is other*/
-    StatusUpdateDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_pol PRIMARY KEY (POLogId),
     CONSTRAINT fk_pol_POId FOREIGN KEY (POId) REFERENCES  PurchaseOrder (POId),
-    CONSTRAINT fk_pol_UserId FOREIGN KEY (UserId) REFERENCES  Users (UserId)
+    CONSTRAINT fk_pol_UserId FOREIGN KEY (POCreatedBy) REFERENCES  Users (UserId)
 );
 
 /*Payment receipt not implemented*/
@@ -303,12 +270,12 @@ CREATE TABLE Asset (
     AssetId INT NOT NULL AUTO_INCREMENT,
     LabId INT, 
     ItemId INT,
+    SerialNo VARCHAR(20), /* unique number given to item will help to track item*/
     QuantityAvailable INT, 
     StorageLocation VARCHAR(100),
     ShortDescription VARCHAR(100),
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     DateModified DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    IsActive BOOLEAN,
     CONSTRAINT pk_a PRIMARY KEY (AssetId),
     CONSTRAINT fk_a_LabId FOREIGN KEY (LabId) REFERENCES  Lab (LabId),
     CONSTRAINT fk_a_ItemId FOREIGN KEY (ItemId) REFERENCES  Item (ItemId)
@@ -317,15 +284,32 @@ CREATE TABLE Asset (
 CREATE TABLE AssetTransactionLog (
     TransactionId INT NOT NULL AUTO_INCREMENT,
     AssetId INT,
+    ActionTakenBy INT, /*UserID who is responsible for the activity, e.g. 'Lab Incharge'*/  
     TransactionAction VARCHAR(50), /*Added, Issued, Returned, Scraped*/
-    POItemId INT, /* only for purchase otherwise NULL*/
-    ActivityLogId INT, /* Only for issued, Returned otherwise NULL*/
     Quantity INT,
     ShortDescription VARCHAR(100),
-    CreatedBy INT, /*userid*/  
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_atl PRIMARY KEY (TransactionId),
     CONSTRAINT fk_atl_AssetId FOREIGN KEY (AssetId) REFERENCES  Asset (AssetId),
-    CONSTRAINT fk_atl_POItemId FOREIGN KEY (POItemId) REFERENCES  POItem (POItemId),
-    CONSTRAINT fk_atl_ActivityLogId FOREIGN KEY (ActivityLogId) REFERENCES  LabActivityLog (ActivityLogId)
+    CONSTRAINT fk_atl_UserId FOREIGN KEY (ActionTakenBy) REFERENCES  Users (UserId)
+);
+
+CREATE TABLE ActivityItemTransaction (
+     /* Once the item is issued or returned, it must be reflected in Asset table for total quantity available for that item */
+    ActivityTrasId INT NOT NULL AUTO_INCREMENT,
+    ActivityId INT,
+    /*RItemID INT,
+    ItemId INT,*/
+    AssetId INT,
+    Requestor INT, /* UserId who raised the request, Student, Individual*/ 
+    ProcessedBy INT, /*UserId  who executed the request, e.g. Lab assistant*/
+    ActionTaken VARCHAR(50), /*Issued, Returned, etc*/
+    ActionDate DATETIME,
+    Quantity INT,
+    ShortDescription VARCHAR(50),
+    CONSTRAINT pk_ait PRIMARY KEY (ActivityTrasId),
+    CONSTRAINT fk_ait_ActivityId FOREIGN KEY (ActivityId) REFERENCES  LabActivity (ActivityId),
+    CONSTRAINT fk_ait_AssetId FOREIGN KEY (AssetId) REFERENCES  Asset (AssetId),
+    CONSTRAINT fk_ait_UserId FOREIGN KEY (Requestor) REFERENCES  Users (UserId),
+    CONSTRAINT fk_ait_UserId1 FOREIGN KEY (ProcessedBy) REFERENCES  Users (UserId)
 );
