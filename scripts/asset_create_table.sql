f5df425841d9644d4a27613aa69e134acebd5038
Kartik Kulkarni <kartik.kulkarni@outlook.com>
2025-04-14 11:21:28 +0530

    Modified Comments

--- scripts/asset_create_table.sql
+++ scripts/asset_create_table.sql
@@ -1,29 +1,71 @@
-/*  For an Activity funds will be allocated accordingly. 
-    The available lab funds will be updated and data will be logged.
-    To carry out activity using some items, Request will be created.
-    The list will be reviewed and the available items will be issued for that activity. 
-    Accordingly Asset inventory will be updated.
-    If the items are not available then, purchase order (PO) will be created against the activity funds. 
-    After approval, when the items are received, the PO will be closed, Funds will be updated.
-    The received items will be added in item list, Asset as well as item lab list. 
-    These item then will be issued for the Activity and the request will be closed.
+/*  For an Activity funds will be allocated 
+    Accordingly, the available lab funds will be updated and data will be logged.
+    Step: 
+        1)  Activity will be created
+        2)  To carry out activity using some items are required.
+            Here if the item required is not available (quantity) or is not listed (unknown) in Item table, 
+            then new item is first created in Item table with IsUnknown = 0. 
+        3)  Request will be created for the user (Student/individual). 
+        4)  The list will be reviewed and will be approved/ rejected
+        5)  After the approval, the available items will be issued for that activity.
+            Asset inventory will be updated and data will be logged.
+        6)  For the items which are not available or not listed (unknown), 
+            then purchase order (PO) will be created against the activity FundsAvailable.
+        7)  After approval for the PO, when the items are received, 
+            the PO will be closed, Activity FundsAvailable will be updated also the data will be logged.
+            Multiple PO can be made as per requirement against activity FundsAvailable.  
+        8)  The received items will be added in Asset table with quantity and serial number will be assigned to it.
+        9)  These item then will be issued for the Activity to user who requested and the request will be closed.
+        9)  Once the activity is complete, 
+            Item will be returned by Student and accordingly asset inventory will be updated for quantity. 
+            The balance FundsAvailable of activity will be refunded (moved/added to lab FundsAvailable)
+            The activity will be closed (IsClosed will be set to 1)
 
-    User table will have records of application users (username, password) as well as student to whom items are issued
-    Students data will be maintained but they will not log in system
+    Assumption:
+        1)  User table will have records of application users (username, password) as well as student to whom items are issued
+            Currently, Students data will be maintained but they will not log in system. 
+            (If access is given then they can only create request)
+        2)  Currently, Prices of the item is not considered in Item table. 
+            If it has to included then for the cost changes, we will need to add log to maintain history of prices for that item, 
+            so that correspondingly every project/activity will have the correct price by correct linking with the purchase order.
+        3)  Setting IsActive = false is for delete action for front end user. 
+            It is soft delete. Record will not deleted actually
+        4)  We will not maintain supplier information in the database for purchases. 
+        5)  Purchase order has been distilled as per requirement. (Only Create, Approve, Close status will be there)
+        6)  Bills will not be maintained for the purchase.
+        7)  Every transaction is created first time and then updated as per progress in main table. 
+            But all these transaction will be logged in their child table as history of all transaction. 
+            History will show the progress for that particular record of main table.
+        
+            We are maintaining history for following tables:
+                (main table)       -  (Child table to maintain transaction history)
+                    Lab            -     LabLog
+                    LabActivity    -     LabActivityLog
+                    Request        -     RequestLog
+                    PurchaseOrder  -     POLog
+                    Asset          -     AssetTransactionLog
 
-    Setting IsActive = false is for delete action for front end user. 
-    It is soft delete. Record will not deleted actually
+        e.g. 
+            a.  Consider Lab (main table) record with initial funds first time. 
+                This will be inserted in Lab and will be inserted in the LabLog (Child table) with action (Added)
+            b.  Now let lab received more funds, then this record will be updated for funds in Lab table 
+                and a new record will be inserted in LabLog table with description and action (Added)
+            c.  If the lab funds need to be allocated to some activity, then same record will be updated in Lab table
+                and a new record will be inserted in LabLog table with description and action (Spent)
     
-    Please be careful for delete action, 
-    Delete action in parent table will affect the related data in child table.
-    for example if an user is deleted,  then its role will be deleted too.  
+    Precaution: 
 
-    Trigger can be implemented for multiple queries based on single add/delete/update
-    or send queries one by one. 
-    We need to commit transaction separately on button click otherwise data won't be saved
-    in database. 
+        1)  Please be careful for delete action, 
+            Delete action in parent table will affect the related data in child table.
+            e.g. if an user is deleted, then its role will be deleted too. 
+                 Also it will affect the record in ActivityItemTransaction table for the item if that user (return) have 
+                 as well as Asset table (quantity of item).
+            Here, Trigger can be implemented for multiple queries based on single add/delete/update
+            or send queries one by one. 
+        2)  We need to commit transaction separately on button click (for insert/update/delete). 
+            Otherwise data won't be saved in database. 
+            Check for rollback if cancel button is clicked.
 
-    We will not maintain supplier information in the database for purchases. Purchase order has been distilled as per requirement.
 */
 
 USE AssetManagement;
@@ -49,7 +91,7 @@ DROP TABLE IF EXISTS Roles;
 
 CREATE TABLE Roles (
     RoleId INT NOT NULL AUTO_INCREMENT,
-    RoleName VARCHAR(50), /* Admin, HOD, Lab incharge, Lab assistant, Student */ 
+    RoleName VARCHAR(50), /* Admin, HOD, Lab incharge (Professor), Lab assistant, Student */ 
     RoleDesc VARCHAR(100),
 	DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     IsActive BOOLEAN,
@@ -86,8 +128,8 @@ CREATE TABLE Department (
 
 CREATE TABLE Lab (
     /* FundsAvailable will be updated if funds are received as well as 
-      if the funds allocated to lab activity are spent, this amount need to be updated.
-      All transaction will be logged in Lablog as history
+       if the funds allocated to lab activity are spent, this amount need to be updated.   
+       All transaction will be logged in Lablog as history
     */
     LabId INT NOT NULL AUTO_INCREMENT,
     DeptId INT,
@@ -101,6 +143,7 @@ CREATE TABLE Lab (
 );
 
 CREATE TABLE UserRole (
+    /* mapping of user, role and lab */
     UserRoleId INT NOT NULL AUTO_INCREMENT,
     UserId INT,
     LabId INT,
@@ -116,7 +159,7 @@ CREATE TABLE UserRole (
 );
 
 CREATE TABLE LabLog (
-     /* There will only be inserts in this table, no updation or deletion against fund transaction done in lab table.*/
+     /* There will only be inserts with action in this table (for any create/update/delete against fund transaction done in Lab table).*/
     LabLogId INT NOT NULL AUTO_INCREMENT,
     LabId INT,
     ActionTakenBy INT, /* UserId who modified lab record for funds (e.g. 'HOD','Lab Incharge') */
@@ -130,7 +173,7 @@ CREATE TABLE LabLog (
 );
 
 CREATE TABLE Item (
-     /* Stores only unique items. And you should check to not allow for duplicate entries. TODO: Add unique constraints. Assume cost of the item is fixed for all time. If the cost changes, then we will need to add log to maintain history of prices for that item, so that correspondingly every project will have the correct price by correct linking with the purchase order. */
+     /* Stores only unique items. And you should check to not allow for duplicate entries */
     ItemId INT NOT NULL AUTO_INCREMENT,
     Category VARCHAR(20), /* Monitor, Keyboard, CPU, Cards, Camera, Motors, etc*/
 	Make VARCHAR(100),
@@ -145,14 +188,21 @@ CREATE TABLE Item (
 );
 
 CREATE TABLE LabActivity (
-    /* Once the purchase done amount in FundsAvailable should be updated. 
+    /* 
     Initially when record is created, FundsAvailable = FundsAllocated. 
-    If the funds are not sufficient, then Request/Purchase order must get rejected.
-    If the activity is closed, the funds available should get added to lab funds.
-    Once more funds are allocated, the funds available and allocated should be updated equally.
-    Lab funds when allocated to Activity then it is subtracted from Lab funds,  
-    and when purchase is made against the activity, 
-    then it is subtracted from activity funds and available funds will be updated.
+    Lab FundsAvailable when allocated to Activity then it is subtracted from Lab FundsAvailable.
+    
+    If the funds are not sufficient, then Request/Purchase order should either get rejected or more funds will be made available.
+    Once more funds are allocated, the FundsAvailable and FundsAllocated should be updated equally.
+      
+    Once the purchase done amount in FundsAvailable should be updated. 
+    If purchase is made against the activity, then it is subtracted from activity FundsAvailable.
+    FundsAvailable will be updated after the completion of Purchase (item received).
+
+    If the activity is closed, then balance fund should be moved to Lab table (add in FundsAvailable),
+    and IsClosed will be set to 1. FundsAvailable in this table can be made 0. 
+    
+    All insertion/updation should be logged in LabActivityLog table
     */  
     ActivityId INT NOT NULL AUTO_INCREMENT,
     LabId INT,
@@ -172,7 +222,8 @@ CREATE TABLE LabActivity (
 );
 
 CREATE TABLE LabActivityLog (
-     /* Once the item is issued or returned, it must be reflected in Asset table for total quantity available for that item */
+    /* There will only be inserts with action in this table 
+    (for any create/update/delete against Activity fund transaction done in LabActivity table).*/
     ActivityLogId INT NOT NULL AUTO_INCREMENT,
     ActivityId INT,
     ActionTakenBy INT, /* (UserId) Processed by  e.g.'Lab Incharge'*/
@@ -200,12 +251,13 @@ CREATE TABLE Request (
 );
 
 CREATE TABLE RequestItem (
+    /* Child table of Request. Mapping of the item which are Requested*/
     /* if item is not listed in item list,
      then create the item in item table and then allocate in requestItem table*/
     RItemID INT NOT NULL AUTO_INCREMENT,
     RequestId INT,
     ItemId INT,
-    IsUnknown BOOLEAN, /* If the item is not listed in item list, then it will be unknown*/
+    IsUnknown BOOLEAN, /* If the item is not listed in item list, then it will be unknown i.e IsUnknown = 1 else 0*/
     QuantityRequested INT,
     DateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_ri PRIMARY KEY (RItemID),
@@ -214,6 +266,7 @@ CREATE TABLE RequestItem (
 );
   
 CREATE TABLE RequestLog (
+    /* Child table of Request. Maintains History of Request transaction*/
     RequestLogId INT NOT NULL AUTO_INCREMENT,    
     RequestId INT,
     ActionTakenBy INT,  /* UserId Processed by e.g. 'Lab Assistance, 'Lab Incharge''*/
@@ -226,6 +279,7 @@ CREATE TABLE RequestLog (
 );
 
 CREATE TABLE PurchaseOrder (
+    /* Stores record for not listed (unknown) items as well as unavailable item  for the activity*/
     POId INT NOT NULL AUTO_INCREMENT,
     ActivityId  INT,
     OrderDate DATETIME,
@@ -239,6 +293,7 @@ CREATE TABLE PurchaseOrder (
 );
 
 CREATE TABLE POItem (
+    /* Child table of PurchaseOrder. Mapping of the item which need to be purchased*/
     POItemId INT NOT NULL AUTO_INCREMENT,
     POId INT,
     ItemId INT,
@@ -251,6 +306,7 @@ CREATE TABLE POItem (
 );
 
 CREATE TABLE POLog (
+    /* Child table of PurchaseOrder. Maintains History of PurchaseOrder transaction*/
     POLogId INT NOT NULL AUTO_INCREMENT,
     POId INT,
     POCreatedBy INT, /*UserId who created/updated PO Lab Assistance/incharge  */ 
@@ -265,7 +321,7 @@ CREATE TABLE POLog (
 /*Payment receipt not implemented*/
 
 CREATE TABLE Asset (
-    /* Once the item is issued or returned, it must be reflected in Asset(updated) 
+    /* Once the item is issued or returned, it must be reflected in Asset (that record needs to be updated) 
     for total quantity available for that item as well as added in AssetTransactionLog table  */
     AssetId INT NOT NULL AUTO_INCREMENT,
     LabId INT, 
@@ -282,6 +338,7 @@ CREATE TABLE Asset (
 );
 
 CREATE TABLE AssetTransactionLog (
+     /* Child table of Asset. Maintains History of Asset transaction*/
     TransactionId INT NOT NULL AUTO_INCREMENT,
     AssetId INT,
     ActionTakenBy INT, /*UserID who is responsible for the activity, e.g. 'Lab Incharge'*/  
